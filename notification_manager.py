6fac4103dbd8db3573084b9bc08688d02f60a858
Anafreisa <65288393+Anafreisa@users.noreply.github.com>
2021-02-19 16:39:54 -0300

    Add files via upload

---
+++ notification_manager.py
@@ -0,0 +1,35 @@
+import smtplib
+from twilio.rest import Client
+
+TWILIO_SID = "AC90eb1f16a02b2c8106bf0414348307f1"
+TWILIO_AUTH_TOKEN = "0e27d5cd86e2f38b1b04c589db26bb05"
+TWILIO_VIRTUAL_NUMBER = "+16504472856"
+TWILIO_VERIFIED_NUMBER = "+5561996879742"
+MAIL_PROVIDER_SMTP_ADDRESS = "smtp.gmail.com"
+MY_EMAIL = "anafreisa.teste@gmail.com"
+MY_PASSWORD = "Coguja20"
+
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        print(message.sid)
+
+    def send_emails(self, emails, message, google_flight_link):
+        with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
+                )
